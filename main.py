14d02fdbb1542eddbd29a27e518f44b8ee23e960
Mina <hominaa99@gmail.com>
2024-06-08 16:02:26 +0330

    fix duplicate data insertion

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import requests_html
 import psycopg2
 import json
-
 from psycopg2.extras import execute_values
-
 from config import DB_CONFIG
 import signal
 import sys
@@ -25,25 +23,34 @@ def connect_to_db():
         return None
 
 
+def generate_insert_query(table_name, columns, values_list):
+    columns_str = ', '.join(columns)
+    values_placeholder = ', '.join(['%s'] * len(values_list))
+    query = f"INSERT INTO {table_name} ({columns_str}) VALUES {values_placeholder} ON CONFLICT (ad_code) DO NOTHING"
+    return query, values_list
+
+
 def insert_ad_data(connection, ads_list):
-    try:
-        cursor = connection.cursor()
-        query = "INSERT INTO ads (ad_data, ad_code) VALUES (%s, %s)"
-        # Extract ad_data and ad_code from ads_list
-        records = [(json.dumps(ad['ad_data']), ad['ad_code']) for ad in ads_list]
-        # Execute the query with executemany
-        result = cursor.executemany(query, records)
+    s = 0
+    table_name = "ads"
+    columns = ["ad_data", "ad_code"]
+    cursor = connection.cursor()
+    query, values = generate_insert_query(table_name, columns, ads_list)
+    cursor.execute(query, values)
+    if cursor.rowcount == 0:
+        s += 1
+    if s >= 3:
+        connection.commit()
         cursor.close()
-        print(result)
-    except Exception as e:
-        print(f"Error inserting data: {e}")
+        return "Ads are repeating..."
+    connection.commit()
+    cursor.close()
 
 
 def scrape_bama_data(url):
     connection = connect_to_db()
     if connection is None:
         return
-    # start_time = time.time()
     try:
         session = requests_html.HTMLSession()
         j = 0
@@ -53,38 +60,33 @@ def scrape_bama_data(url):
                 r.raise_for_status()
                 js = r.json()
                 ads_list = []
-                ads = js['data']['ads']  # [ 0, 1 , ... 30]
+                ads = js['data']['ads']
                 if not ads:
                     break
-                for i in ads:  # data0 , data1 , ... , data30
+                for i in ads:
                     if i['type'] == 'ad':
-                        code = i['detail']['code']  # code0, code1
-                        ads_list.append({'ad_data': i, 'ad_code': code})
+                        code = i['detail']['code']
+                        i = json.dumps(i)
+                        ads_list.append((i, code))
                     else:
                         continue
                 insert_ad_data(connection, ads_list)
-                if j == 5:
+                if j == 10:
                     break
                 j += 1
             except KeyboardInterrupt:
                 print("KeyboardInterrupt detected. Exiting...")
                 sys.exit(0)
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                break
     finally:
-        # end_time = time.time()
-        # execution_time = end_time - start_time
-        # print(f"Execution time: {execution_time} seconds")
         if connection:
             connection.close()
 
 
-# def sigterm_handler(signum, frame):
-#     print("SIGTERM received. Exiting...")
-#     sys.exit(0)
+def sigterm_handler(signum, frame):
+    print("SIGTERM received. Exiting...")
+    sys.exit(0)
 
 
-# signal.signal(signal.SIGTERM, sigterm_handler)
+signal.signal(signal.SIGTERM, sigterm_handler)
 bama_url = 'https://bama.ir/cad/api/search'
 scrape_bama_data(bama_url)
